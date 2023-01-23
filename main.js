33aac25689990a33ea9236271b76d1eaa1029073
ivan-vanlev <vanlevbox@gmail.com>
2023-01-23 11:21:48 +0200

    first attempt

---
+++ main.js
@@ -0,0 +1,86 @@
+
+
+// let a = 'JavaScript';
+// let b = 33;
+// let c = true;
+// let d = false;
+// let e = NaN;
+// let f = undefined;
+
+
+
+
+// console.log(`Example #1: '${a}' + ${b} = ${a + b} (число 33 преобразуется к строке)`);
+// console.log(`Example #2: '${a}' + ${c} = ${a + c} (булевое значение преобразуется к строке)`);
+// console.log(`Example #3: '${a}' * ${b} = ${a * b} (операции со строкой кроме '+' возвращают NaN)`);
+// console.log(`Example #4: '${a}' - ${c} = ${a - c} (операции со строкой кроме '+' возвращают NaN)`);
+// console.log(`Example #5: ${b} + ${c} = ${b + c} (булевое 'true' становится 1)`);
+// console.log(`Example #6: ${b} - ${d} = ${b - d} (булевое 'false' становится 0)`);
+// console.log(`Example #7: ${b} + ${e} = ${b + e} (любые операции с NaN возвращают NaN)`);
+// console.log(`Example #8: '${a}' + ${f} = ${a + f} (undefined преобразуется в строку)`);
+// console.log(`Example #9: ${b} + ${f} = ${b + f} (любые операции с undefined, кроме сложения со строкой возвращают NaN)`);
+// console.log(`Example #10: ${c} + ${d} = ${c + d} (true преобразуется к 1, false становится 0)`);
+// console.log(`Example #11: ${e} + ${f} = ${e + f} (любые операции с NaN возвращают NaN)`);
+// console.log(`Example #12: '${a}' + ${b} + ${c} = ${a + b + c} (все операнды преобразовались к строке)`);
+// console.log(`Example #13: '${a}' + ${b} - ${c} = ${a + b - c} ('JavaScript33 - 1 = NaN')`);
+
+
+
+
+// const a = 3;
+// const b = '2a';
+// console.log(a + b); // 32a - автоматическое приведение
+// console.log(Number(a) + Number(b)); // NaN - явное приведение
+
+
+// const a = '223';
+// const b = 823;
+
+// console.log(Number(a)); 
+// console.log(String(b)); 
+
+
+
+// const a = NaN;
+// const b = undefined;
+// let c = null;
+// let d = 4;
+
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(1 + b);
+
+
+// console.log(Boolean('0'));
+// console.log(typeof false);
+
+// let age = '312';
+// let dage = '333';
+// console.log(Number(age) + Number(dage) ); 
+
+// console.log(13 % 3);
+// console.log(2 ** 4);
+
+// let counter = 15;
+// counter++; // работает как counter = counter + 1, просто запись короче
+// console.log(counter); 
+
+
+// console.log(typeof NaN); 
+
+
+a1 = 2;
+x = 2;
+x++;
+h = ++x;
+a2 = 2;
+b = ++a1; // a1=3, b=3
+c = a2++; // a2=3, c=2
+q = ++c;
+console.log(h);
+console.log(c);
+
+// console.log('99' - 200 + 10);
+// console.log('y' + 1 + 0);
